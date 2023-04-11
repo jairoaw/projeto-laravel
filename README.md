5582bba322cffde4e793ad6c1b1e21a178617100
jairoaw <omesmojairo@gmail.com>
2023-04-11 20:33:04 -0300

    edit: criação da rota estoque

--- README.md
+++ README.md
@@ -4,6 +4,16 @@
 ```sh
 composer create-project laravel/laravel projeto
 
+#inicia o servidor, usar o powershell
 php artisan serve
+
+#criar o controller
+php artisan make:controller Estoque
+
+#irá criar o arquivo em App\Http\Controllers
+
+
+
+
 
 ```
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\EstoqueController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,10 +18,18 @@ Route::get('/', function () {         //:: operador de reducao de escopo
     return view('welcome');         //renderiza a pagina que está na pasta views
 });
 
-Route::get('/teste', function () {
-    return 'o teste funcionou';
-});
+Route::get('/estoque', [EstoqueController::class, 'index']);      //o primeiro parametro é a própria classe e o segundo é a funcao que vai ser executada dentro da classe (index)
 
-Route::get('/teste-com-view', function () {
-    return view('teste');
-});
+
+
+// Route::get('/teste', function () {
+//     return 'o teste funcionou';
+// });
+
+// Route::get('/teste-com-view', function () {
+//     return view('teste');
+// });
+
+// Route::get('/noticia/{id}', function ($id) {
+//     return "voce está lendo a noticia {$id}";
+// });
