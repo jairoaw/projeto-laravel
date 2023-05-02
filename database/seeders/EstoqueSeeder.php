69fe40469daaf147a8cd41d6de48ee65cf60e984
jairoaw <omesmojairo@gmail.com>
2023-05-02 17:55:17 -0300

    edit: seeders

---
+++ database/seeders/EstoqueSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class EstoqueSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('estoques')->insert([
+            [
+                'nome' => 'Bermuda',
+                'quabntidade' => 30,
+            ],
+            [
+                'nome' => 'Camiseta',
+                'quabntidade' => 50,
+            ],
+            
+        ]);
+    }
+}
